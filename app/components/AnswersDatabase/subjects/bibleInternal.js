5b1ee50c38233f1cbd00f7ba8603a811e2741f0d
michaelcheng429 <cheng@csdisco.com>
2017-08-17 13:31:36 -0500

    add questions

--- app/components/AnswersDatabase/subjects/bibleInternal.js
+++ app/components/AnswersDatabase/subjects/bibleInternal.js
@@ -16,6 +16,223 @@ const bibleInternal = [
     {
         added: '8/17/2017',
         updated: '',
+        title: 'Is the Bible contradictory about Ahaziah\'s age when he began to rule?',
+        tags: ['bible-internal'],
+        url: 'http://christianreformedanswers.com/is-the-bible-contradictory-about-ahaziahs-age-when-he-began-to-rule/'
+    },
+    {
+        added: '8/17/2017',
+        updated: '',
+        title: 'Is the Bible contradictory about all humans being sinners?',
+        tags: ['bible-internal'],
+        url: 'http://christianreformedanswers.com/is-the-bible-contradictory-about-all-humans-being-sinners/'
+    },
+    {
+        added: '8/17/2017',
+        updated: '',
+        title: 'Is the Bible contradictory about calling people a "fool"?',
+        tags: ['bible-internal'],
+        url: 'http://christianreformedanswers.com/is-the-bible-contradictory-about-calling-people-a-fool/'
+    },
+    {
+        added: '8/17/2017',
+        updated: '',
+        title: 'Is the Bible contradictory about children being punished for their parents\' sin?',
+        tags: ['bible-internal'],
+        url: 'http://christianreformedanswers.com/is-the-bible-contradictory-about-children-being-punished-for-their-parents-sin/'
+    },
+    {
+        added: '8/17/2017',
+        updated: '',
+        title: 'Is the Bible contradictory about God accepting human sacrifice?',
+        tags: ['bible-internal'],
+        url: 'http://christianreformedanswers.com/is-the-bible-contradictory-about-god-accepting-human-sacrifice/'
+    },
+    {
+        added: '8/17/2017',
+        updated: '',
+        title: 'Is the Bible contradictory about God being a God of peace or war?',
+        tags: ['bible-internal'],
+        url: 'http://christianreformedanswers.com/is-the-bible-contradictory-about-god-being-a-god-of-peace-or-war/'
+    },
+    {
+        added: '8/17/2017',
+        updated: '',
+        title: 'Is the Bible contradictory about God being good or evil?',
+        tags: ['bible-internal'],
+        url: 'http://christianreformedanswers.com/is-the-bible-contradictory-about-god-being-good-or-evil/'
+    },
+    {
+        added: '8/17/2017',
+        updated: '',
+        title: 'Is the Bible contradictory about God changing his mind?',
+        tags: ['bible-internal'],
+        url: 'http://christianreformedanswers.com/is-the-bible-contradictory-about-god-changing-his-mind/'
+    },
+    {
+        added: '8/17/2017',
+        updated: '',
+        title: 'Is the Bible contradictory about God living in light vs. darkness?',
+        tags: ['bible-internal'],
+        url: 'http://christianreformedanswers.com/is-the-bible-contradictory-about-god-living-in-light-vs-darkness/'
+    },
+    {
+        added: '8/17/2017',
+        updated: '',
+        title: 'Is the Bible contradictory about God\'s omnipotence?',
+        tags: ['bible-internal'],
+        url: 'http://christianreformedanswers.com/is-the-bible-contradictory-about-gods-omnipotence/'
+    },
+    {
+        added: '8/17/2017',
+        updated: '',
+        title: 'Is the Bible contradictory about good works being seen?',
+        tags: ['bible-internal'],
+        url: 'http://christianreformedanswers.com/is-the-bible-contradictory-about-works-being-seen/'
+    },
+    {
+        added: '8/17/2017',
+        updated: '',
+        title: 'Is the Bible contradictory about God tempting people?',
+        tags: ['bible-internal'],
+        url: 'http://christianreformedanswers.com/is-the-bible-contradictory-about-god-tempting-people/'
+    },
+    {
+        added: '8/17/2017',
+        updated: '',
+        title: 'Is the Bible contradictory about graven images?',
+        tags: ['bible-internal'],
+        url: 'http://christianreformedanswers.com/is-the-bible-contradictory-about-graven-images/'
+    },
+    {
+        added: '8/17/2017',
+        updated: '',
+        title: 'Is the Bible contradictory about how many animals went on the ark?',
+        tags: ['bible-internal'],
+        url: 'http://christianreformedanswers.com/is-the-bible-contradictory-about-how-many-animals-went-on-the-ark/'
+    },
+    {
+        added: '8/17/2017',
+        updated: '',
+        title: 'Is the Bible contradictory about how many Gods there are?',
+        tags: ['bible-internal'],
+        url: 'http://christianreformedanswers.com/is-the-bible-contradictory-about-how-many-gods-there-are/'
+    },
+    {
+        added: '8/17/2017',
+        updated: '',
+        title: 'Is the Bible contradictory about how many stalls of horses Solomon had?',
+        tags: ['bible-internal'],
+        url: 'http://christianreformedanswers.com/is-the-bible-contradictory-about-how-many-stalls-of-horses-solomon-had/'
+    },
+    {
+        added: '8/17/2017',
+        updated: '',
+        title: 'Is the Bible contradictory about Jesus bearing witness about himself?',
+        tags: ['bible-internal'],
+        url: 'http://christianreformedanswers.com/is-the-bible-contradictory-about-jesus-bearing-witness-about-himself/'
+    },
+    {
+        added: '8/17/2017',
+        updated: '',
+        title: 'Is the Bible contradictory about Jesus bringing peace?',
+        tags: ['bible-internal'],
+        url: 'http://christianreformedanswers.com/is-the-bible-contradictory-about-jesus-bringing-peace/'
+    },
+    {
+        added: '8/17/2017',
+        updated: '',
+        title: 'Is the Bible contradictory about Joseph being Jesus\' father?',
+        tags: ['bible-internal'],
+        url: 'http://christianreformedanswers.com/is-the-bible-contradictory-about-joseph-being-jesus-father/'
+    },
+    {
+        added: '8/17/2017',
+        updated: '',
+        title: 'Is the Bible contradictory about lying?',
+        tags: ['bible-internal'],
+        url: 'http://christianreformedanswers.com/is-bible-contradictory-lying/'
+    },
+    {
+        added: '8/17/2017',
+        updated: '',
+        title: 'Is the Bible contradictory about men and women being equal?',
+        tags: ['bible-internal'],
+        url: 'http://christianreformedanswers.com/is-the-bible-contradictory-about-men-and-women-being-equal/'
+    },
+    {
+        added: '8/17/2017',
+        updated: '',
+        title: 'Is the Bible contradictory about obeying the government?',
+        tags: ['bible-internal'],
+        url: 'http://christianreformedanswers.com/is-the-bible-contradictory-about-obeying-the-government/'
+    },
+    {
+        added: '8/17/2017',
+        updated: '',
+        title: 'Is the Bible contradictory about people seeing God?',
+        tags: ['bible-internal'],
+        url: 'http://christianreformedanswers.com/is-the-bible-contradictory-about-people-seeing-god/'
+    },
+    {
+        added: '8/17/2017',
+        updated: '',
+        title: 'Is the Bible contradictory about salvation by faith or works?',
+        tags: ['bible-internal'],
+        url: 'http://christianreformedanswers.com/is-the-bible-contradictory-about-salvation-by-faith-or-works/'
+    },
+    {
+        added: '8/17/2017',
+        updated: '',
+        title: 'Is the Bible contradictory about stealing?',
+        tags: ['bible-internal'],
+        url: 'http://christianreformedanswers.com/is-bible-contradictory-stealing/'
+    },
+    {
+        added: '8/17/2017',
+        updated: '',
+        title: 'Is the Bible contradictory about swearing oaths?',
+        tags: ['bible-internal'],
+        url: 'http://christianreformedanswers.com/is-the-bible-contradictory-about-swearing-oaths/'
+    },
+    {
+        added: '8/17/2017',
+        updated: '',
+        title: 'Is the Bible contradictory about the Sabbath?',
+        tags: ['bible-internal'],
+        url: 'http://christianreformedanswers.com/is-the-bible-contradictory-about-the-sabbath/'
+    },
+    {
+        added: '8/17/2017',
+        updated: '',
+        title: 'Is the Bible contradictory about when Jesus was crucified?',
+        tags: ['bible-internal'],
+        url: 'http://christianreformedanswers.com/is-the-bible-contradictory-about-when-jesus-was-crucified/'
+    },
+    {
+        added: '8/17/2017',
+        updated: '',
+        title: 'Is the Bible contradictory about when trees/plants were created?',
+        tags: ['bible-internal', 'creation'],
+        url: 'http://christianreformedanswers.com/is-the-bible-contradictory-about-when-trees-plants-were-created/'
+    },
+    {
+        added: '8/17/2017',
+        updated: '',
+        title: 'Is the Bible contradictory about whether Michal had children?',
+        tags: ['bible-internal', 'creation'],
+        url: 'http://christianreformedanswers.com/is-the-bible-contradictory-about-whether-michal-had-children/'
+    },
+    {
+        added: '8/17/2017',
+        updated: '',
+        title: 'Is the Bible contradictory about whether Paul\'s men heard a voice?',
+        tags: ['bible-internal', 'creation'],
+        url: 'http://christianreformedanswers.com/is-the-bible-contradictory-about-whether-pauls-men-heard-a-voice/'
+    },
+    {
+        added: '8/17/2017',
+        updated: '',
         title: 'Should Christians kill? (Is it "You shall not murder" or "You shall not kill"?',
         tags: ['bible-internal'],
         url: 'http://christianreformedanswers.com/should-christians-kill-is-it-you-shall-not-murder-or-you-shall-not-kill/'
