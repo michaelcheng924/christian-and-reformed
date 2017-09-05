7057ab5fd199b3559defd69eef8e013416fc6176
michaelcheng429 <cheng@csdisco.com>
2017-09-05 13:21:29 -0500

    add question

--- app/components/AnswersDatabase/subjects/science.js
+++ app/components/AnswersDatabase/subjects/science.js
@@ -5,6 +5,13 @@ const science = [
         title: "Does science contradict the Bible?",
         tags: ['science'],
         url: 'http://christianreformedanswers.com/does-science-contradict-the-bible/'
+    },
+    {
+        added: '9/5/2017',
+        updated: '',
+        title: 'Is the burden of proof upon Christians?',
+        tags: ['science'],
+        url: 'http://christianreformedanswers.com/is-the-burden-of-proof-upon-christians/'
     }
 ];
 
